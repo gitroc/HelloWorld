6642848d264d79b3b3d898b1e8b4030df91a7e8e
song <songjunpeng@greenlandfinacial.com>
2018-06-08 14:55:14 +0800

    Ui State测试。

--- Ui.js
+++ Ui.js
@@ -4,7 +4,29 @@ import { Text, View } from 'react-native';
 class Greeting extends Component {
     render() {
         return (
-            <Text>Hello {this.props.name}!</Text>
+            <Text>Hello {this.props.name}!, Your ages is {this.props.age}</Text>
+        );
+    }
+}
+
+class Blink extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { showText: true };
+
+        // 每1000毫秒对showText状态做一次取反操作
+        setInterval(() => {
+            this.setState(previousState => {
+                return { showText: !previousState.showText };
+            });
+        }, 1000);
+    }
+
+    render() {
+        // 根据当前showText的值决定是否显示text内容
+        let display = this.state.showText ? this.props.text : ' ';
+        return (
+            <Text>{display}</Text>
         );
     }
 }
@@ -15,7 +37,11 @@ export default class Ui extends Component {
             <View style={{alignItems: 'center'}}>
                 <Greeting name='Rexxar' />
                 <Greeting name='Jaina' />
-                <Greeting name='Valeera' />
+                <Greeting name='Valeera' age='100' />
+                <Blink text='I love to blink' />
+                <Blink text='Yes blinking is so great' />
+                <Blink text='Why did they ever take this out of HTML' />
+                <Blink text='Look at me look at me look at me' />
             </View>
         );
     }
