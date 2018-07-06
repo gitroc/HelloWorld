c3b76034f3ae5a58d51bed8175b9bb4169ec28f8
songjunpeng <rocday@163.com>
2018-07-06 09:31:35 +0800

    refine.

---
+++ app/resource/share/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import weChatSession from './wechat-session.png';
+import weChatTimeLine from './wechat-timeLine.png';
+import qq from './qq.png';
+import copy from './copy.png';
+
+const Share = {
+    weChatSession: weChatSession,
+    weChatTimeLine: weChatTimeLine,
+    qq: qq,
+    copy: copy
+};
+
+export default {
+    Share
+};
